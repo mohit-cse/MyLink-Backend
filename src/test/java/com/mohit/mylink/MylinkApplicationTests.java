6924fc924fc815691da184cded554ef53805218a
Mohit Sharma <bmohits0203@gmail.com>
2024-07-27 15:44:40 +0530

    first commit

---
+++ src/test/java/com/mohit/mylink/MylinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.mylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
