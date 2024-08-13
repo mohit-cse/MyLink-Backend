9d7a38b50391a87005d5f92014689fb51b1b915a
Mohit Sharma <bmohits0203@gmail.com>
2024-08-13 21:19:12 +0530

    Updated config for services

--- DocService/src/main/java/com/mohit/docservice/Utils/Exceptions/GlobalExceptionHandler.java
+++ DocService/src/main/java/com/mohit/docservice/Utils/Exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.mohit.docservice.Utils.Exceptions;
 
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> fileTooLarge(MaxUploadSizeExceededException e) {
+        e.printStackTrace();
+        return new ResponseEntity<>("Max allowed size for document is 10MB", HttpStatus.REQUEST_ENTITY_TOO_LARGE);
+    }
     @ExceptionHandler(MultipartException.class)
     public ResponseEntity<String> noAttachment(MultipartException e) {
         e.printStackTrace();
