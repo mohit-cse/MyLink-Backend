477c9668b19402185ec4813fdf83e6f0dca86f0f
Mohit Sharma <bmohits0203@gmail.com>
2024-09-15 15:56:52 +0530

    added swagger config

--- NotificationService/cmd/main.go
+++ NotificationService/cmd/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"github.com/mohit-cse/MyLink-Backend/NotificationService/controllers"
+	_ "github.com/mohit-cse/MyLink-Backend/NotificationService/docs"
 	"github.com/mohit-cse/MyLink-Backend/NotificationService/kafka_consumer"
 )
 
+// @title 	Notification Service
+// @version	1.0
+// @description Service for delivering notifications
 func main() {
 	kafkaConsumer := kafka_consumer.KafkaConsumer{}
 	kafkaConsumer.Initialize()
-	go kafkaConsumer.StartKafkaSubscriber()
+	// go kafkaConsumer.StartKafkaSubscriber()
 	restServer := controllers.NotificationController{}
 	restServer.Initialize()
 	go restServer.StartServer()
--- NotificationService/controllers/notification.go
+++ NotificationService/controllers/notification.go
@@ -1,32 +1,23 @@
 package controllers
 
 import (
-	"encoding/json"
 	"errors"
 	"log"
 	"net/http"
 	"strconv"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 	"github.com/mohit-cse/MyLink-Backend/NotificationService/configs"
 	"github.com/mohit-cse/MyLink-Backend/NotificationService/modals"
 	"github.com/mohit-cse/MyLink-Backend/NotificationService/services"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type NotificationController struct {
 	config  *configs.RestConfig
-	router  *mux.Router
+	router  *gin.Engine
 	service *services.NotificationService
-}
-
-type loggingResponseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (l *loggingResponseWriter) WriteHeader(statusCode int) {
-	l.statusCode = statusCode
-	l.ResponseWriter.WriteHeader(statusCode)
 }
 
 func (cont *NotificationController) Initialize() {
@@ -38,61 +29,57 @@ func (cont *NotificationController) Initialize() {
 
 func (cont *NotificationController) StartServer() {
 	log.Println("Started HTTP Server")
-	err := http.ListenAndServe(":"+strconv.Itoa(cont.config.ServerPort), cont.logRequest(cont.router))
+	err := cont.router.Run(":" + strconv.Itoa(cont.config.ServerPort))
 	if errors.Is(err, http.ErrServerClosed) {
 		log.Println("Server Closed")
-		panic(err)
-	} else if err != nil {
 		panic(err)
 	}
 }
 
-func (cont *NotificationController) logRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		l := &loggingResponseWriter{ResponseWriter: w}
-		handler.ServeHTTP(l, r)
-		log.Printf("%s %s %s %s\n", r.RemoteAddr, r.Method, strconv.Itoa(l.statusCode), r.URL)
-	})
-}
-
-func (cont *NotificationController) createRouter() *mux.Router {
-	router := mux.NewRouter()
-	router.HandleFunc("/api/notify", cont.sendNotification).Methods("POST")
+func (cont *NotificationController) createRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.POST("/api/notify", cont.sendNotification)
 	return router
 }
 
-func (cont *NotificationController) sendNotification(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Content-Type") != "application/json" {
-		http.Error(w, "Invalid Content-Type. Only application/json is allowed.", http.StatusBadRequest)
+// CreateTags		godoc
+// @Summary			Send notification
+// @Description		Sends notification to mentioned channels
+// @Param			request body modals.NotificationRequest true "Send notification"
+// @Produce			application/json
+// @Tags			tags
+// @Success			200 {object}  modals.NotificationResponse{}
+// @Router			/api/notify [post]
+func (cont *NotificationController) sendNotification(c *gin.Context) {
+	if c.Request.Header.Get("Content-Type") != "application/json" {
+		c.String(http.StatusBadRequest, "Invalid Content-Type. Only application/json is allowed.")
 		return
 	}
 
 	var request modals.NotificationRequest
-	err := json.NewDecoder(r.Body).Decode(&request)
+	err := c.ShouldBindJSON(&request)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
 		response := modals.NotificationResponse{IsDelivered: false, Message: "Invalid payload"}
-		json.NewEncoder(w).Encode(&response)
+		c.JSON(http.StatusBadRequest, response)
 		log.Println(err)
 		return
 	}
 
 	isValid, message := cont.service.ValidateRequest(&request)
 	if !isValid {
-		w.WriteHeader(http.StatusBadRequest)
 		response := modals.NotificationResponse{IsDelivered: false, Message: message}
-		json.NewEncoder(w).Encode(&response)
+		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
 	response, status := cont.service.SendNotification(&request)
+	http_status := http.StatusOK
 	switch status {
 	case "partially_delivered":
-		w.WriteHeader(http.StatusPartialContent)
+		http_status = http.StatusPartialContent
 	case "failed":
-		w.WriteHeader(http.StatusInternalServerError)
-	case "success":
-		w.WriteHeader(http.StatusOK)
+		http_status = http.StatusInternalServerError
 	}
-	json.NewEncoder(w).Encode(response)
+	c.JSON(http_status, response)
 }
